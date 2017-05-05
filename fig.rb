4470a9284b9a5c357276ac2493e1c310bce5c969
Ashur Cabrera <ashur@panic.com>
2017-05-05 15:49:29 -0700

    fig 0.3.1

--- fig.rb
+++ fig.rb
@@ -2,8 +2,8 @@ class Fig < Formula
   desc "Quickly build and deploy configuration profiles."
   homepage "https://github.com/ashur/fig"
   url "https://github.com/ashur/fig.git",
-    :tag => "v0.3.0",
-    :revision => "b2439d5aa3dfc940c950089a36506c723a017ae8"
+    :tag => "v0.3.1",
+    :revision => "1d29ade44e199aa6c9f93f7e9c83a88cf5995994"
   head "https://github.com/ashur/fig.git"
 
   def install
